103fe8044a8ec58882247487e9c755ae0838fd1f
Harb-A <harbabbas02@gmail.com>
2022-12-11 03:58:30 +0200

    Backend - Projects table migration

--- Taskify-Backend/taskify/database/migrations/2022_12_11_014216_create_projects_table.php
+++ Taskify-Backend/taskify/database/migrations/2022_12_11_014216_create_projects_table.php
@@ -15,6 +15,9 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('user-id');
             $table->timestamps();
         });
     }
