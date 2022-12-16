6c6c539275479a1461261b6c100f37eb73a21277
Harb-A <harbabbas02@gmail.com>
2022-12-16 19:32:43 +0200

    Backend - Updated location replace

--- Taskify - Frontend/Scripts/register.js
+++ Taskify - Frontend/Scripts/register.js
@@ -10,7 +10,7 @@ window.onload = () => {
       const res = await axios.post("http://localhost:8000/api/register", data);
       if (res.data.status == "success") {
         alert("Registered account successfuly!");
-        form.reset();
+        location.replace("http://127.0.0.1:5500/Pages/login.html");
       } else {
         alert("Email already exists!");
       }
