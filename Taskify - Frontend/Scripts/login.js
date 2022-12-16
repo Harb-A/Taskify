aac5a222b83f362d1150d06e3a373094161c9f24
Harb-A <harbabbas02@gmail.com>
2022-12-16 20:45:34 +0200

    Frontend - Added theme change

--- Taskify - Frontend/Scripts/login.js
+++ Taskify - Frontend/Scripts/login.js
@@ -1,16 +1,16 @@
 window.onload = () => {
-  // let themeLink = document.getElementById("theme-link");
-  // let themeSelect = document.getElementById("theme");
+  let themeLink = document.getElementById("theme-link");
+  let themeSelect = document.getElementById("theme");
 
-  // const themeSwitch = () => {
-  //   if (themeSelect.value === "1") {
-  //     themeLink.href = "../Styles/login.css";
-  //   } else if (themeSelect.value === "2") {
-  //     themeLink.href = "../Styles/login-dark.css";
-  //   }
-  // };
+  const themeSwitch = () => {
+    if (themeSelect.value === "1") {
+      themeLink.href = "../Styles/login.css";
+    } else if (themeSelect.value === "2") {
+      themeLink.href = "../Styles/login-dark.css";
+    }
+  };
 
-  // themeSelect.onchange = themeSwitch;
+  themeSelect.onchange = themeSwitch;
 
   const form = document.getElementById("loginForm");
 
